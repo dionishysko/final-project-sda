fcae39b8422333899eaeeb10ff69ddd04e1c913b
dionishysko <dionishysko94@gmail.com>
2024-05-24 21:53:59 +0200

    Bej push Henri e u kry

--- final-project-sda/src/App.js
+++ final-project-sda/src/App.js
@@ -1,13 +1,20 @@
-
-import './App.css';
-import logo from './assets/images/backgroundimage.jpg';
+import "./App.css";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Work from "./components/Work.js/Work";
+import Testimonial from "./components/Testimonial/Testimonial";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/footer";
 
 function App() {
   return (
     <div className="App">
-    
-        <img src={logo} className="App-logo" alt="logo"/>
-      
+      <Home />
+      <About />
+      <Work />
+      <Testimonial />
+      <Contact />
+      <Footer />
     </div>
   );
 }

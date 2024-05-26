296f3b3af95fd3271ca1d32e4ea661956bf5f499
FlavioHoxha2023 <149088052+FlavioHoxha2023@users.noreply.github.com>
2024-05-26 11:24:01 +0200

    Add new changes about login page

--- final-project-sda/src/components/Login/Login.js
+++ final-project-sda/src/components/Login/Login.js
@@ -1,6 +1,52 @@
-const Login =()=>{
-    return(
-        <h3>Login to create</h3>
-    )
-}
+import React, { useState } from 'react';
+import  { Link }  from 'react-router-dom';
+import './Login.css';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    
+    
+    console.log('Logging in with:', email, password);
+  };
+
+  return (
+    <div className="login-container">
+      <div className="login-photo">
+       {/* <img src="foodLogIn.jpg" alt="Login" /> */}
+      </div>
+      <div className="login-form">
+        <h2>Log In</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+            <label>Email:</label>
+            <input 
+              type="email" 
+              value={email} 
+              onChange={(e) => setEmail(e.target.value)} 
+              required 
+            />
+          </div>
+          <div>
+            <label>Password:</label>
+            <input 
+              type="password" 
+              value={password} 
+              onChange={(e) => setPassword(e.target.value)} 
+              required 
+            />
+          </div>
+          <button type="submit">Log In</button>
+        </form>
+        <p>
+          Don't have an account?
+        </p>
+      </div>
+    </div>
+  );
+};
+
 export default Login;
